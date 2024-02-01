37b77b6dbe1d16047a0ecd2f258f40f0b3fe88a6
HarrisM00 <Harrismahmood014@gmail.com>
2024-02-01 14:11:32 -0500

    trial and error

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var 1893
+function addParagraph() {
+    // Create a new paragraph element
+    var newParagraph = document.createElement("p");
+    
+    // Set the content for the new paragraph
+    newParagraph.textContent = "This is a new paragraph.";
+
+    // Append the new paragraph to the container
+    document.getElementById("paragraphContainer").appendChild(newParagraph);
+}
